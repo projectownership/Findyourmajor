e4237a0b53980b65a84541ebc565a02813c8a800
projectownership <demingjr@gmail.com>
2026-06-24 15:14:41 -0400

    Update Quiz.jsx
    fix stripe link

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react"; 
 import { useNavigate } from "react-router-dom";
 import { NAVY, AMBER, AMBER_L, SLATE, OFFWHT, WHITE, GREEN, INDIGO, PURPLE } from "../brand.js";
 import { Analytics } from "../brand.js";
